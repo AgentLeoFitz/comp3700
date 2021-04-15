24219f5f65a9ba7ee96302917dcca6c785d00ae2
Daniel <dlc0050@auburn.edu>
2021-04-15 13:13:47 -0500

    adsfgfs

--- game.py
+++ game.py
@@ -1,4 +1,5 @@
 # coding=UTF-8
+from group import *
 
 class game(object):
 
@@ -12,10 +13,41 @@ class game(object):
   """ ATTRIBUTES
 
    
-
+  name   (protected)
   genre  (protected)
 
   """
+  def __init__(self, nameIn)
+    global name = nameIn
+    global players = []
+    
+  def start(self):
+    """
+     
+
+    @return bool :
+    @author
+    """
+    pass
+
+  def addPlayers(self, playerList):
+    """
+     
+
+    @param list playerList : 
+    @return bool :
+    @author
+    """
+    players.add(playerList)
+
+  def endGame(self):
+    """
+     
+
+    @return bool :
+    @author
+    """
+    pass
 
 
 
