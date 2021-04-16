93aef8a0289bb9c54b71f87f1a0289c0fbdb4b28
Daniel <dlc0050@auburn.edu>
2021-04-15 20:52:34 -0500

    more tictactoe stuff

--- tictactoe.py
+++ tictactoe.py
@@ -33,4 +33,43 @@ class tictactoe(game):
         else:
             global icon = 'x'
             
-        
+    def winCheck(self):
+        # Check x
+        for i in range(0,2):
+            sum = 0
+            col = [row[i] for row in board]
+            for j in range(0,2)
+                if col[j] == 'x'
+                    sum += 1
+                if sum == 3:
+                    return 'x'
+            row = board[i]
+            for j in range(0,2)
+                if row[j] == 'x'
+                    sum += 1
+                if sum == 3:
+                    return 'x'
+        # Check o
+        for i in range(0,2):
+            sum = 0
+            col = [row[i] for row in board]
+            for j in range(0,2)
+                if col[j] == 'o'
+                    sum += 1
+                if sum == 3:
+                    return 'o'
+            row = board[i]
+            for j in range(0,2)
+                if row[j] == 'o'
+                    sum += 1
+                if sum == 3:
+                    return 'o'
+        if board[0][0] == 'x' and board[1][1] == 'x' and board[2][2] == 'x':
+            return 'x'
+        if board[0][2] == 'x' and board[1][1] == 'x' and board[2][0] == 'x':
+            return 'x'
+        if board[0][0] == 'o' and board[1][1] == 'o' and board[2][2] == 'o':
+            return 'o'
+        if board[0][2] == 'o' and board[1][1] == 'o' and board[2][0] == 'o':
+            return 'o'
+        return ''
