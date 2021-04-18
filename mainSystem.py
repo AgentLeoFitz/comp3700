545748e4fa2f9f520d4dfbfac5e23ca7bd1924cf
Daniel <dlc0050@auburn.edu>
2021-04-18 17:10:43 -0500

    now we cooking with gas bois!

--- mainSystem.py
+++ mainSystem.py
@@ -1,21 +1,42 @@
 import game
 import group
 import user
+import threading
 from hashlib import sha512
-
-def main():
-    global allGames
-    allGames = []
-    global userDatabase
-    userDatabase = dict()
-    createUser("user","secure")
+import tkinter as tk
 
 def createUser(userName,password):
     if userName in userDatabase:
         print("USER ALREADY EXISTS")
         return False
     hashGen = sha512()
-    hashGen.update(password)
-    hash = hashGen.hexdigest()
-    userDatabase.update({userName,hash})
+    hashGen.update(bytearray(password, encoding ='utf-8'))
+    pHash = hashGen.hexdigest()
+    userDatabase.update({userName:pHash})
     return True
+
+def yeet():
+    print("YEEET!")
+    
+class mainMenu(tk.Frame):
+   goButt = None
+   newButt = None
+   def __init__(self, root):
+      tk.Frame.__init__(self, root)
+      self.root = root
+      root.title("Log In")
+      self.inButt = tk.Button(root, text = 'Login', fg = 'forest green', command = yeet).place(relx = .5, rely = .5, anchor = 'center')
+      
+def main():
+    global allGames
+    allGames = []
+    global userDatabase
+    
+    userDatabase = dict()
+    createUser("user","secure")
+    root = tk.Tk()
+    box = mainMenu(root)
+    box.mainloop()
+
+if __name__ == "__main__":
+    main()
