190c609eba81a125cc67950d5c15ae1bca980b4d
njh01 <66970692+njh01@users.noreply.github.com>
2021-04-22 00:21:52 -0500

    Add files via upload
    shit don't work

--- mainSystem.py
+++ mainSystem.py
@@ -32,6 +32,36 @@ def logIn(userName, password):
 
 
 class dash(tk.Frame):
+
+    def createDash(groupName):
+        group = group.__init__(groupName)
+        tk.Frame.__init__(self, parentWin.root)
+        self.root = tk.Tk()
+        self.root.title(owner.getName())
+        tk.Label(self.root,text="Admin Menu").grid(row=0, column=0)
+        tk.Label(self.root, text="Player: ").grid(row=1, column=0)
+        tk.Label(self.root, text="Privacy: ").grid(row=2, column=0)
+        tk.Label(self.root, text="Genre: ").grid(row=3, column=0)
+        tk.Label(self.root, text="Join Code: ").grid(row=4, column=0)
+        tk.Label(self.root, text="Language: ").grid(row=5, column=0)
+        tk.Label(self.root, text="Region: ").grid(row=6, column=0)
+        player = tk.Entry(self.root)
+        player.grid(row=1, column=1)
+        privacy = tk.Entry(self.root)
+        privacy.grid(row=2, column=1)
+        genre = tk.Entry(self.root)
+        genre.grid(row=3, column=1)
+        joinCode = tk.Entry(self.root)
+        joinCode.grid(row=4, column=1)
+        language = tk.Entry(self.root)
+        language.grid(row=5, column=1)
+        region = tk.Entry(self.root)
+        region.grid(row=1, column=1)
+        inButt = tk.Button(self.root, text="Set Attributes", fg="forest green", command=group.setAttributes(player,privacy,genre,joinCode,language,region)).grid(row=2, column=0)
+
+    def joinDash(groupName):
+        return 0
+
     def __init__(self, parentWin, owner):
         tk.Frame.__init__(self, parentWin.root)
         self.root = tk.Tk()
@@ -40,9 +70,10 @@ class dash(tk.Frame):
         tk.Label(self.root, text="Group Name: ").grid(row=1, column=0)
         groupName = tk.Entry(self.root)
         groupName.grid(row=1, column=1)
-        inButt = tk.Button(self.root, text="Create Group", fg="forest green", command=group.__init__(str(groupName))).grid(row=2, column=0)
-        newButt = tk.Button(self.root, text="Join Group", fg="red", command=group._addWaitlist(str(groupName))).grid(row=2, column=1)
+        inButt = tk.Button(self.root, text="Create Group", fg="forest green", command=self.createDash(str(groupName))).grid(row=2, column=0)
+        newButt = tk.Button(self.root, text="Join Group", fg="red", command=self.joinDash(str(groupName))).grid(row=2, column=1)
         self.mainloop()
+
 
 
 class mainMenu(tk.Frame):
